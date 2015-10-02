b7e363e7076bb35611458aa104194f68119e4479
Shilpa Nanja <nanja17@up.edu>
2015-10-02 13:49:09 -0700

    allow users to choose charts for different years

--- public/ui.js
+++ public/ui.js
@@ -32,7 +32,22 @@ var initialize = function() {
     // toggle function shall be invoked.                                                                                           
     button.onclick = toggle;
 
+	 // Grab the 'Submit' button element, identified by the                                       
+    // 'submit-btn' id.                                                                          
+    button = document.getElementById('submit-btn');
+
+    // From this point forward, when the button is clicked, the                                 
+    // fetch function shall be invoked.                                                          
+    button.onclick = fetch;
 };
+
+var fetch = function() {
+	
+	var year_element = document.getElementById('year');
+	var year_value = year_element.value;
+	console.log(year_value);
+	vizController(year_value);
+}
 
 // When this file is included at the bottom of the page,                                                                           
 // the js is loaded after the DOM is loaded.  It is a                                                                              
--- public/viz.js
+++ public/viz.js
@@ -13,10 +13,88 @@
 
 google.load('visualization', '1', {packages: ['corechart']});
 
-google.setOnLoadCallback(drawChart);
+google.setOnLoadCallback(vizInit);
+// Define the variables to hold the entire fusion table,                                                               
+// and a collection of views, one for each year.  
+var data; 
+var views = {};
+var totals = {};
+// Define the variable to hold the chart.                                                                              
+var chart;
+// At the start of execution, the year is 2013-2014, the most                                                          
+// recent academic year that has 12 months of data.  To represent                                                      
+// an academic year, use a pair of values.                                                                             
+var year = [2013, 2014];
+var options = {
+ 	width: 1000,
+    height: 563,
+	title: 'Session Hours Provided by University of Portland Librarians in 2014',
+	hAxis: {
+		title: 'Month',
+		gridlines: {count: 12}
+	},
+	animation: {
+		"startup" : true,
+		"duration" : 500
+	},
+	 vAxis: {
+		title: 'People Hours'
+	},
+	legend: {
+		position: 'none'
+	}
+};
+function vizInit() {
+// Create a new viz object using the google API -- specifically,
+// we are going to make a column chart inside the div called ex0                                                   
+// in the html file.
+chart = new google.visualization.ColumnChart(document.getElementById('ex0'));
 
-function drawChart() {
 
+// 9/19/2015 Corrected typo
+// Make the initial query to get the whole Fusion table. The Fusion
+// table’s ID is listed in red.                                                            
+var query = "SELECT Month, Year, AY, Sessions FROM 1P23PE35fnBA8V9Bf4u2C3jqqwr-O0i-s8pjrSEjD";
+
+var opts = {sendMethod: 'auto'};
+var queryObj = new google.visualization.Query('https://www.google.com/fusiontables/gvizdata?tq=', opts);
+
+
+// Send the query and handle the response by logging the data
+// to the console.                                                                
+queryObj.setQuery(query);
+queryObj.send(function(e) {
+       
+	data = e.getDataTable();
+
+    console.log(data);
+
+    // Create a view for academic year 2013-2014 that                                                          
+    // is the first two columns of the data, just the                                                          
+    // rows that have 2013-2014 for the value.                                                                 
+
+    // First, get the textualized range of the year.                                                           
+	var thisYear = "" + year[0] + "-" + year[1];
+
+	// Next, create the object and get the rows 
+	// corresponding to "thisYear".                                   
+	views[thisYear] = new google.visualization.DataView(data);
+           
+	views[thisYear].setRows(views[thisYear].getFilteredRows([{column: 2, value: thisYear}]));
+
+	// Get a subset of the columns.                                                                            
+	views[thisYear].setColumns([0, 3]);
+
+	// Draw the chart for the initial academic year.                                                           
+	chart.draw(views[thisYear].toDataTable(), options);
+	
+	
+
+	
+});
+
+
+/*
     // STEP 3: STORE THE DATA.
 
     // Store the data by creating a google DataTable object with
@@ -45,25 +123,7 @@ function drawChart() {
     // Set the options for the chart to be drawn.  This include the
     // width, height, title, horizontal axis, vertical axis.  Finally
     // turn off the legend.
-    var options = {
-        width: 700,
-        height: 400,
-	title: 'Session Hours Provided by University of Portland Librarians in 2014',
-        hAxis: {
-            title: 'Month',
-            gridlines: {count: 12}
-        },
-        vAxis: {
-            title: 'People Hours'
-        },
-	legend: { 
-	    position: 'none' 
-	},
-	animation: {
-		"startup" : true,
-		"duration" : 500
-	},
-    };
+
 
     // Create a new viz object using the google API -- specifically,
     // we are going to make a column chart inside the div called ex0
@@ -73,4 +133,28 @@ function drawChart() {
     // STEP 7: SHOW THE DATA
     // Draw the chart with the supplied options.
     chart.draw(data, options);
+    
+    */
+}
+
+function vizController(thisYear) {
+	
+		//if view[thisYear] doesn't exist, create a 
+		//dataView object to get the subset of data for the year
+		if((views[thisYear]) == null){
+			// Next, create the object and get the rows 
+		// corresponding to "thisYear".                                   
+		views[thisYear] = new google.visualization.DataView(data);
+           
+		views[thisYear].setRows(views[thisYear].getFilteredRows([{column: 2, value: thisYear}]));
+
+		// Get a subset of the columns.                                                                            
+		views[thisYear].setColumns([0, 3]);
+		}
+
+    // Draw the chart for the academic year.                                                           
+	chart.draw(views[thisYear].toDataTable(), options);
+    console.log(thisYear);
+
+
 }
